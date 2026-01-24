91660286691175d62c77a6128119b6fb4db5ebcd
gustoy1212 <137191082+gustoy1212@users.noreply.github.com>
2026-01-23 21:23:43 -0300

    Update script_auto_8426a598.lua

--- discord_uploads/script_auto_8426a598.lua
+++ discord_uploads/script_auto_8426a598.lua
@@ -1,8 +1,8 @@
 -- ARQUIVO NO GITHUB (whitelist.lua)
 return {
-    -- ["123456789"] = ID_DO_COMPRADOR
+    
     
     ["123456789"] = 8207477640, 
-    ["KEY"] = 99887766,
+    ["gudiel"] = 2285697382,
     ["KEY-ADM-TESTE"] = 55443322
 }
